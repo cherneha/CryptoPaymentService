41be2253a368e8f9c0fefb6a4c349a72c427153e
cherneha <staceychernega@gmail.com>
2018-11-04 12:27:39 +0200

    token with erc20 standard

---
+++ microservice/contracts/ContractManager.py
@@ -0,0 +1,7 @@
+from web3.contract import C
+#
+# class TokenManager:
+#     def __init__(self, address):
+#         self.address = address
+#         self.contract = contract.Contract(self.address)
+#
--- token/milo.sol
+++ token/milo.sol
@@ -1,32 +1,52 @@
 pragma solidity ^0.4.0;
 contract Milo{
     address owner;
-    mapping (address => uint64) public balances;
-    string name;
-    uint256 supply;
+    mapping (address => uint) public balances;
+    mapping(address => mapping (address => uint)) allowed;
+    
+    string public constant name = "Milo";
+    uint public constant supply = 1000000;
+    uint8 public constant decimals = 0;
     
     constructor() public{
-        name = "Milo";
         owner = msg.sender;
         balances[owner] = 1000000;
-        supply = 1000000;
     }
     
-    function totalSupply() public constant returns (uint256 a){
-        a = supply;
-        return a;
+    function totalSupply() public view returns (uint a){
+        return supply;
     }
     
-    function transfer(address to, address from, uint64 quantity) public{
+    function transferFrom(address from, address to, uint tokens) public returns (bool success){
+        success = false;
         require(0x0 != to, "You do not want to send to 0 address!");
-        require(balances[from] >= quantity, "Not enough MILO");
-        
-        balances[from] = balances[from] - quantity;
-        balances[to] = balances[to] + quantity;
+        require(balances[from] >= tokens, "Not enough MILO");
+        require(allowed[from][to] >= tokens, "Not allowed to transfer this amount");
+        allowed[from][to] = allowed[from][to] - tokens;
+        balances[from] = balances[from] - tokens;
+        balances[to] = balances[to] + tokens;
+        return true;
     }
     
-    function balance(address account) public view returns (uint64 accountBalance){
+    function balance(address account) public view returns (uint accountBalance){
         accountBalance = balances[account];
         return accountBalance;
+    }
+    
+    function transfer(address to, uint tokens) public returns (bool success){
+        success = false;
+        require(0x0 != to, "You do not want to send to 0 address!");
+        require(balances[msg.sender] >= tokens, "Not enough MILO");
+        balances[msg.sender] = balances[msg.sender] - tokens;
+        balances[to] = balances[to] + tokens;
+        return true;
+    }
+    function approve(address spender, uint tokens) public returns (bool success){
+        success = false;
+        allowed[msg.sender][spender] = allowed[msg.sender][spender] + tokens;
+        return true;
+    }
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining){
+        return allowed[tokenOwner][spender];
     }
 }
